012af5018f2377d4ad21d4106112007c4a1cf7ec
llueca <cllueca6@gmail.com>
2023-01-20 17:12:23 +0100

    Creada primera vista, query de prueba y un poco de templates

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.db import connection, transaction
 
-# Create your views here.
+
+# Funcion que convierte las querys a la BBDD en diccionarios, para acceder de manera mas facil al valor de cada campo en las templates
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+        dict(zip(columns, row))
+        for row in cursor.fetchall()
+    ]
+
+
+def paginaPrincipal(request):
+
+    try:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM "Usuarios";')
+        data = dictfetchall(cursor)
+    except Exception as e:
+        print("Ha ocurrido un error en la consulta a la BBDD {}".format(e))
+    finally:
+        cursor.close()
+    
+
+    context = {'datos': data}
+    return render(request, 'ecommerce/inicio.html', context)
